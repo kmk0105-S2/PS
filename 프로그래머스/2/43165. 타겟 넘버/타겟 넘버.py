39de63aa83737a0729056aee050232c044344649
kmk0105-S2 <127574815+kmk0105-S2@users.noreply.github.com>
2026-06-30 18:28:18 +0900

    [level 2] Title: 타겟 넘버, Time: 179.98 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,9 +1,15 @@
 def solution(numbers, target):
-    def dfs(index, total):
-        if index == len(numbers):
-            if total == target:
-                return 1
-            else:
-                return 0
-        return dfs(index+1, total+numbers[index]) + dfs(index+1, total-numbers[index])
-    return dfs(0, 0)
+    count = 0
+    
+    def dfs(index, answer):
+        nonlocal count
+        if index == len(numbers):       # 숫자 다 썼으면
+            if answer == target:        # 최종 합이 타겟이면
+                count += 1              # 경우의 수 +1
+            return
+        
+        dfs(index + 1, answer + numbers[index])   # 이 숫자 +로
+        dfs(index + 1, answer - numbers[index])   # 이 숫자 -로
+    
+    dfs(0, 0)
+    return count
