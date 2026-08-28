8ba916038252d27706e9b67ee63d12ff2747a2b0
kmk0105-S2 <127574815+kmk0105-S2@users.noreply.github.com>
2026-08-29 01:30:39 +0900

    [level 2] Title: 소수 찾기, Time: 3.16 ms, Memory: 11.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -0,0 +1,19 @@
+from itertools import permutations
+
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+def solution(numbers):
+    nums = set()
+    for i in range(1, len(numbers) + 1):
+        for p in permutations(numbers, i):
+            nums.add(int(''.join(p)))
+    
+    return sum(1 for n in nums if is_prime(n))
+
+            
